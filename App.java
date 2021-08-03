9bef8dba19d736cfd4b4630c6da8b47fe184e6a2
Ratheesh1986 <76087168+Ratheesh1986@users.noreply.github.com>
2021-08-03 23:14:59 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World .. madhu ." );
+        System.out.println( "Hello World .. Ratheesh ." );
     }
 
 	public int calculateSomething() {
